5f136779fe6b06c116e0137fde77a5e62cfc1612
MakeMoney212002 <115358435+MakeMoney212002@users.noreply.github.com>
2025-09-09 11:27:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ USERNAME = environ.get('USERNAME', "") # ADMIN USERNAME
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001852781486').split()]
 
 # ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002874199652'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002874199652 -1002852051467'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002874199652'))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002852051467'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002852051467')) 
@@ -43,7 +43,7 @@ FILES_DATABASE = environ.get('FILES_DATABASE', "mongodb+srv://Ganesh:212002@nekk
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002779791560'))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', ''))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002779791560') 
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
